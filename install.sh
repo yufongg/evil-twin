37b0156b045a8acdcaaf337dcc59de797aac3a85
yf <73434276+yufongg@users.noreply.github.com>
2022-01-19 06:35:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 apt install aircrack-ng
 apt install dnschef
 apt install isc-dhcp-server
+apt install gnome-terminal
 unzip instagram.zip
 chmod 777 html
 echo "Transfer html files to your webserver dir"
